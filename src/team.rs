981772eb8d929a19a3dd4e457b42e85740820e72
Tanya Srinidhi <srinidhi.tanya@gmail.com>
2023-05-15 09:14:51 +1000

    feat: add structs for parsing V7 dataset exports

--- src/team.rs
+++ src/team.rs
@@ -1,12 +1,12 @@
 #[allow(unused_imports)]
 use fake::{Dummy, Fake};
 
+use crate::annotation::AnnotationClass;
 use crate::expect_http_ok;
 use anyhow::{bail, Context, Result};
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use serde_yaml::Value;
-use std::collections::HashMap;
 use std::{fmt::Display, path::PathBuf};
 
 use crate::client::V7Methods;
@@ -43,6 +43,43 @@ impl Display for TeamMember {
     }
 }
 
+#[derive(Debug, Default, Clone, Serialize, Deserialize, Dummy, PartialEq, Eq)]
+pub struct TypeCount {
+    pub count: u32,
+    pub id: Option<u32>,
+    pub name: Option<String>,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize, Dummy)]
+pub struct TeamAnnotationClasses {
+    pub annotation_classes: Vec<AnnotationClass>,
+    pub type_counts: Vec<TypeCount>,
+}
+
+impl TryFrom<(&Value, &Value)> for Team {
+    type Error = anyhow::Error;
+
+    fn try_from(value: (&Value, &Value)) -> Result<Self, Self::Error> {
+        let slug = value.0.as_str().context("Invalid team slug")?.to_string();
+        let api_key: Option<String> = match value.1.get("api_key") {
+            Some(key) => Some(key.as_str().context("Invalid api-key")?.to_string()),
+            None => None,
+        };
+        let datasets_dir: Option<PathBuf> = match value.1.get("datasets_dir") {
+            Some(key) => Some(PathBuf::from(
+                key.as_str().context("Invalid datasets_dir")?.to_string(),
+            )),
+            None => None,
+        };
+
+        Ok(Self {
+            slug,
+            datasets_dir,
+            api_key,
+            team_id: None,
+        })
+    }
+}
 #[async_trait]
 pub trait TeamDescribeMethods<C>
 where
@@ -159,209 +196,10 @@ where
     // }
 }
 
-#[derive(Debug, Default, Clone, Serialize, Deserialize, Dummy, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, Dummy, PartialEq, Eq)]
 pub struct MetadataSkeleton {
     #[serde(rename = "_type")]
     pub skeleton_type: String,
-}
-
-#[derive(Debug, Default, Clone, Serialize, Deserialize, Dummy, PartialEq, Eq)]
-pub struct AnnotationClassMetadata {
-    #[serde(rename = "_color")]
-    pub color: String,
-    pub polygon: Option<HashMap<String, String>>, // TODO find out what this type actually is
-    pub auto_annotate: Option<HashMap<String, String>>, // TODO find out what this type actually is
-    pub inference: Option<HashMap<String, String>>, // TODO find out what this type actually is
-    pub measures: Option<HashMap<String, String>>, // TODO find out what this type actually is
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, Dummy, PartialEq, Eq)]
-#[serde(rename_all = "lowercase")]
-pub enum AnnotationType {
-    Attributes,
-    #[serde(rename = "auto_annotate")]
-    AutoAnnotate,
-    #[serde(rename = "bounding_box")]
-    BoundingBox,
-    Cuboid,
-    #[serde(rename = "directional_vector")]
-    DirectionalVector,
-    Ellipse,
-    Inference,
-    #[serde(rename = "instance_id")]
-    InstanceId,
-    Keypoint,
-    Line,
-    Measures,
-    Polygon,
-    Skeleton,
-    Tag,
-    Text,
-}
-
-// Various ids for annotation types and sub types
-// Tag: 1
-// Tag-Attributes: [5 1]
-// Tag-Text: [6, 1]
-// Tag-Attributes-Text: [5, 6, 1]
-// Polygon: 3
-// Polygon-Attributes: [5, 3]
-// Polygon-Text: [6, 3]
-// Polygon-DirectionalVector: [6 4 3]
-// Polygon-InstanceId: [9 3]
-// bbox: 2
-// bbox-attributes: [2 5]
-// bbox-tag: [2, 6]
-// skeleton: 12
-// skeleton-text: [12 6]
-// line: 11
-// line-text-instanceid: [6 9 11]
-// keypoint: 7
-// ellipse: 60
-// cuboid: 8
-
-impl From<AnnotationType> for u32 {
-    fn from(value: AnnotationType) -> u32 {
-        match value {
-            AnnotationType::Attributes => 5,
-            AnnotationType::AutoAnnotate => todo!(),
-            AnnotationType::BoundingBox => 2,
-            AnnotationType::Cuboid => todo!(),
-            AnnotationType::DirectionalVector => todo!(),
-            AnnotationType::Ellipse => todo!(),
-            AnnotationType::Inference => todo!(),
-            AnnotationType::InstanceId => todo!(),
-            AnnotationType::Keypoint => todo!(),
-            AnnotationType::Line => 11,
-            AnnotationType::Measures => todo!(),
-            AnnotationType::Polygon => 3,
-            AnnotationType::Skeleton => 12,
-            AnnotationType::Tag => 1,
-            AnnotationType::Text => 6,
-        }
-    }
-}
-
-#[derive(Debug, Default, Clone, Serialize, Deserialize, Dummy, PartialEq, Eq)]
-pub struct AnnotationDataset {
-    pub id: u32,
-}
-
-#[derive(Debug, Default, Clone, Serialize, Deserialize, Dummy, PartialEq, Eq)]
-pub struct AnnotationClass {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub annotation_class_image_url: Option<String>,
-
-    #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub annotation_types: Vec<AnnotationType>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub annotation_type_ids: Option<Vec<u32>>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub dataset_id: Option<u32>,
-
-    // #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub datasets: Vec<AnnotationDataset>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub id: Option<u32>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub team_id: Option<u32>,
-
-    // #[serde(skip_serializing_if = "Option::is_none")]
-    pub description: Option<String>,
-
-    // #[serde(skip_serializing_if = "Option::is_none")]
-    pub images: Vec<String>, // TODO: find out what this type is
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub inserted_at: Option<String>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub metadata: Option<AnnotationClassMetadata>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub name: Option<String>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub updated_at: Option<String>,
-}
-
-impl AnnotationClass {
-    pub async fn update<C>(&self, client: &C) -> Result<AnnotationClass>
-    where
-        C: V7Methods,
-    {
-        let endpoint = format!(
-            "annotation_classes/{}",
-            self.id.context("Annotation class is missing an id")?
-        );
-        let response = client.put(&endpoint, Some(&self)).await?;
-
-        expect_http_ok!(response, AnnotationClass)
-    }
-
-    pub async fn delete<C>(&self, client: &C) -> Result<()>
-    where
-        C: V7Methods,
-    {
-        let endpoint = format!(
-            "annotation_classes/{}",
-            self.id.context("Annotation class is missing an id")?
-        );
-
-        let response = client.delete::<AnnotationClass>(&endpoint, None).await?;
-
-        if response.status() != 204 {
-            bail!(format!(
-                "Invalid status code {} {}",
-                response.status(),
-                response.text().await?
-            ));
-        }
-
-        Ok(())
-    }
-}
-
-#[derive(Debug, Default, Clone, Serialize, Deserialize, Dummy, PartialEq, Eq)]
-pub struct TypeCount {
-    pub count: u32,
-    pub id: Option<u32>,
-    pub name: Option<String>,
-}
-
-#[derive(Debug, Default, Clone, Serialize, Deserialize, Dummy, PartialEq, Eq)]
-pub struct TeamAnnotationClasses {
-    pub annotation_classes: Vec<AnnotationClass>,
-    pub type_counts: Vec<TypeCount>,
-}
-
-impl TryFrom<(&Value, &Value)> for Team {
-    type Error = anyhow::Error;
-
-    fn try_from(value: (&Value, &Value)) -> Result<Self, Self::Error> {
-        let slug = value.0.as_str().context("Invalid team slug")?.to_string();
-        let api_key: Option<String> = match value.1.get("api_key") {
-            Some(key) => Some(key.as_str().context("Invalid api-key")?.to_string()),
-            None => None,
-        };
-        let datasets_dir: Option<PathBuf> = match value.1.get("datasets_dir") {
-            Some(key) => Some(PathBuf::from(
-                key.as_str().context("Invalid datasets_dir")?.to_string(),
-            )),
-            None => None,
-        };
-
-        Ok(Self {
-            slug,
-            datasets_dir,
-            api_key,
-            team_id: None,
-        })
-    }
 }
 
 pub mod helpers {
