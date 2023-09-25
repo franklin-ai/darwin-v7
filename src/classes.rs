5053d58092a549b6df7046b8e62b1d955bdd9033
peasee <98815791+peasee@users.noreply.github.com>
2023-09-25 04:53:13 +0000

    fix: Change x,y in BoundingBox f32 as well

--- src/classes.rs
+++ src/classes.rs
@@ -4,8 +4,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize, Dummy, PartialEq)]
 pub struct BoundingBox {
-    pub x: i32,
-    pub y: i32,
+    pub x: f32,
+    pub y: f32,
     pub w: f32,
     pub h: f32,
 }
