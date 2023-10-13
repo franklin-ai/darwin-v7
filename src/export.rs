09fd187405751b042257016fd37dacb6c09c4d19
Tanya Srinidhi <srinidhi.tanya@gmail.com>
2023-10-13 14:12:23 +1100

    fix: specify annotation names for export

--- src/export.rs
+++ src/export.rs
@@ -1,7 +1,7 @@
 //! This file contains structures and methods that define the Darwin Export Format
 //! https://docs.v7labs.com/v1.0/reference/darwin-json
 
-use crate::annotation::AnnotationType;
+use crate::annotation::{BoundingBox, Polygon, Tag, Text};
 use crate::item::DatasetItemTypes;
 use serde::{Deserialize, Serialize};
 
@@ -48,14 +48,18 @@ pub struct ImageAnnotation {
     pub annotators: Option<Vec<Annotator>>,
     // An optional list of reviewers of the image
     pub reviewers: Option<Vec<Annotator>>,
-
-    //// The actual data of the annotation
-    #[serde(alias = "bounding_box", alias = "cuboid")]
-    #[serde(alias = "skeleton", alias = "tag")]
-    pub annotation_type_1: Option<AnnotationType>,
-
-    #[serde(alias = "keypoint", alias = "polygon", alias = "complex_polygon")]
-    pub annotation_type_2: Option<AnnotationType>,
+    // Annotation Type
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub bounding_box: Option<BoundingBox>,
+    // Annotation Type
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tag: Option<Tag>,
+    // Annotation Type
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub polygon: Option<Polygon>,
+    // Annotation Type
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text: Option<Text>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -123,90 +127,6 @@ pub struct JsonExportV2 {
 mod tests {
     use super::*;
     use anyhow::Result;
-
-    #[test]
-    fn test_v7_annotation_export_lines() -> Result<()> {
-        let raw_json = r#"
-                {
-                  "annotators": [
-                    {
-                      "email": "abc.xyz@cheese.com",
-                      "full_name": "ABC XYZ"
-                    }
-                  ],
-                  "id": "fb81c35c-716a-413a-81e8-16ae9c054490",
-                  "bounding_box": {
-                    "h": 588.75,
-                    "w": 630.9500000000116,
-                    "x": 88527.01,
-                    "y": 11805.9
-                  },
-                  "complex_polygon": {
-                    "path": [
-                    [
-                      {
-                        "x": 89094.67,
-                        "y": 11924.8
-                      }], [
-                      {
-                        "x": 89094.67,
-                        "y": 11924.8
-                      }]]
-                  },
-                  "name": "something"
-                }
-        "#;
-        let annotation: ImageAnnotation = serde_json::from_str(raw_json)?;
-        match annotation.annotation_type_2 {
-            Some(AnnotationType::ComplexPolygon(_)) => {}
-            _ => {
-                assert!(false);
-            }
-        };
-        Ok(())
-    }
-
-    #[test]
-    fn test_full_v7_export_file() -> Result<()> {
-        let contents = r#"
-        {
-          "dataset": "Test Dataset",
-          "image": {
-            "filename": "xxxx-xxxx-xxxx-xxxx-xxxx.xxxx",
-            "height": 88149,
-            "original_filename": "xxxxx-xxxx-xxxx-xxxx-xxxx.xxxx",
-            "path": "/",
-            "thumbnail_url": "https://darwin.v7labs.com/api/images/999/thumbnail",
-            "url": "https://darwin.v7labs.com/api/images/999/original",
-            "width": 188688,
-            "workview_url": "https://darwin.v7labs.com/workview?dataset=999&image=54"
-          },
-          "annotations": [
-            {
-              "bounding_box": {
-                "h": 588.75,
-                "w": 630.9500000000116,
-                "x": 88527.01,
-                "y": 11805.9
-              },
-              "id": "770e4a19-a350-4d5e-964e-783512a508f9",
-              "name": "Cheese",
-              "polygon": {
-                "path": [
-                  {
-                    "x": 89094.67,
-                    "y": 11924.8
-                  }]
-              }
-            }
-          ]
-        }
-        "#;
-        let export: JsonExport = serde_json::from_str(contents).expect("Error parsing V7 Export");
-        assert!(export.annotations[0].annotation_type_2.is_some());
-        Ok(())
-    }
-
     #[test]
     fn test_full_v7_export_v2_file() -> Result<()> {
         let contents = r#"
@@ -257,11 +177,12 @@ mod tests {
               "id": "770e4a19-a350-4d5e-964e-783512a508f9",
               "name": "Cheese",
               "polygon": {
-                "path": [
+                "paths": [[
                   {
                     "x": 89094.67,
                     "y": 11924.8
-                  }]
+                  }
+                ]]
               },
 
               "reviewers": [
@@ -280,7 +201,10 @@ mod tests {
         "#;
         let export: JsonExportV2 = serde_json::from_str(contents).expect("Error parsing V7 Export");
         assert_eq!(export.version, "2.0");
-        assert!(export.annotations[0].annotation_type_2.is_some());
+        assert_eq!(
+            export.annotations[0].polygon.clone().unwrap().paths.len(),
+            1
+        );
         Ok(())
     }
 }
